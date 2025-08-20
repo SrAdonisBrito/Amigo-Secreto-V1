50b85c410ffcbf0bc2a6601153f73b20d6e5e985
SrAdonisBrito <adonisjavierbrito@gmail.com>
2025-08-20 13:11:24 -0400

    Terminando las funciones

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
 let listaAmigos =[];
-let nombresAmigos = document.getElementById("amigos");
+let sortear = 0;
+let nombresAmigos = document.getElementById("amigo");
+let mostrarListaAmigos = document.getElementById("listaAmigos");
+let resultado = document.getElementById("resultado");
 
 //Funcion para recibir los nombre escritos.
-function agregarAmigos () {
-    
+function agregarAmigo() {
+    if (!nombresAmigos.value){
+    alert ("Escribe un nombre valido.");
+    };
+
+    listaAmigos.push(nombresAmigos.value);
+    mostrarListaAmigos.innerHTML += `<li> ${nombresAmigos.value}</li>`;
 };
+
+//Funcion adivinar el nombre del amigo.
+function sortearAmigo(){
+    sortear = Math.floor(Math.random() * listaAmigos.length);
+    let amigoSecreto = listaAmigos[sortear];
+    resultado.innerHTML = `<li> El amigo secreto es: ${amigoSecreto} </li>`;
+};
+
